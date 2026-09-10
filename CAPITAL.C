0f7042ab49242cf04251d4ece368788394e4123a
dhruvikkiyada3456 <dhruvikkiyada3456@gmail.com>
2026-09-10 14:37:40 +0530

    Add files via upload

---
+++ CAPITAL.C
@@ -0,0 +1,11 @@
+void main()
+{
+	char x;
+	clrscr();
+	printf("\n enter any capital letter :");
+	scanf("%c",&x);
+
+	printf("\n enter any capital letter %c",x+32);
+
+	getch();
+}
---
+++ SMALL.C
@@ -0,0 +1,11 @@
+void main()
+{
+	char x;
+	clrscr();
+	printf("\n enter any small letter :");
+	scanf("%c",&x);
+
+	printf("\n enter any small letter %c",x-32);
+
+	getch();
+}
